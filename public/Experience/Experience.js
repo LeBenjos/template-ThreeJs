0aed8403a2e74f78d9a65019242806b956aba9dd
LeBenjos <benjamin.schinkel01@outlook.fr>
2023-08-04 20:19:15 +0200

    build: Experience

--- public/Experience/Experience.js
+++ public/Experience/Experience.js
@@ -1,5 +1,31 @@
+import Sizes from "./Utils/Sizes"
+import Time from "./Utils/Time"
+
 export default class Experience{
-    constructor(){
-        console.log('experience start')
+    constructor(canvas){
+        // Save the instance in window
+        window.experience = this
+
+        // Properties
+        this.canvas = canvas
+        this.sizes = new Sizes()
+        this.time = new Time()
+
+        // Listen events
+        this.sizes.on('resize', () => {
+            this.resize()
+        })
+
+        this.time.on('tick', () => {
+            this.update()
+        })
+    }
+
+    resize(){
+        console.log('resize')
+    }
+
+    update(){
+        console.log('update')
     }
 }
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
 import Experience from "../Experience/Experience";
 
 // Start the ThreeJs Experience
-const experience = new Experience()
+const experience = new Experience(document.querySelector(".webgl"))
